e90feb1743939bda06a0cbee5a44d82cf4033f46
Hyrschall <Hyrschall@gmail.com>
2019-02-13 21:35:39 -0700

    I think this might be a small sucess.

---
+++ Lesson_1/Lesson_1/Lesson_1.py
@@ -0,0 +1,11 @@
+my_message = input("Hello World, how are you? ")
+print("Wow! " + my_message + ". That is great to hear!")
+
+my_number = input("How old are you? ")
+print("OMGee! " + my_number + "! You're older than dirt!")
+
+my_platform = input("What platform do you run? ")
+print("I agree " + my_platform + " is the best ever!")
+
+my_computer = input("What brand of PC do you have? ")
+print(my_computer + " ooo ... you should send that back to the manufacture.")
