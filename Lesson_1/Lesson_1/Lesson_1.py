db892f23a9cc149b8e1bfd1da0ef776f410bceb3
Hyrschall <Hyrschall@gmail.com>
2019-02-13 23:01:14 -0700

    can calculate life in seconds

--- Lesson_1/Lesson_1/Lesson_1.py
+++ Lesson_1/Lesson_1/Lesson_1.py
@@ -4,6 +4,13 @@ print("Wow! " + my_message + ". That is great to hear!")
 my_number = input("How old are you? ")
 print("OMGee! " + my_number + "! You're older than dirt!")
 
+num = int(my_number)
+
+seconds_old = num * 12 * 365 * 24 * 60 * 60
+
+sec_old = str(seconds_old)
+print("Your " + sec_old + " seconds old! That's a big number!")
+
 my_platform = input("What platform do you run? ")
 print("I agree " + my_platform + " is the best ever!")
 
