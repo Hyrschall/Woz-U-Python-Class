86b9f95ab657afa54d42dc447b1c1533b6929295
Hyrschall <Hyrschall@gmail.com>
2019-02-23 18:19:05 -0700

    Part 1 lesson 4 completed.

--- lesson_four/main/main.py
+++ lesson_four/main/main.py
@@ -1,37 +1,22 @@
 # Lesson 4 Hands-On with Dictionaries
-fido = {'Name': 'Fido', 'Type': 'Dog', 'Color': 'Black', 'Nickname': 'DoeDoe', 'Owner': 'Joe'}
+fido = {
+    'Name': 'Fido',
+    'Type': 'Dog',
+    'Color': 'Black',
+    'Nickname': 'DoeDoe',
+    'Owner': 'Joe'
+}
 
-charlie = {'Name': 'Charlie', 'Type': 'Turtle', 'Color': 'Green', 'Nickname': 'Slow Poke', 'Owner': 'John'}
+charlie = {
+    'Name': 'Charlie',
+    'Type': 'Turtle',
+    'Color': 'Green',
+    'Nickname': 'Slow Poke',
+    'Owner': 'John'
+}
 
 pets = [fido, charlie]
-# pets['Pet1'] = fido
-# pets['Pet2'] = charlie
 
-# for key in pets:
-#    print(key['Name'] + ':' + key['Type'])
-
-print(list(pets))
-
-# print(pets)
-
-# people = {
-#    'person1': {
-#        'name': 'Sally Sue',
-#        'city': 'Phoenix'
-#    },
-#    'person2': {
-#        'name': 'Billy Bob',
-#        'city': 'Scottsdale'
-#    },
-#    'person3': {
-#        'name': 'Rover',
-#        'city': 'Zappa'
-#    }
-# }
-
-# greetings = []
-
-# for info in people.values():
-#    greetings.append("Hello " + info['name'] + " from " + info['city'])
-
-# print(greetings)
+for pet in pets:
+    for key in pet:
+        print('{} : {}'.format(key, pet[key]))
