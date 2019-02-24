28f245811552ee2d4a5ef12f1712959951916d43
Hyrschall <Hyrschall@gmail.com>
2019-02-23 18:45:02 -0700

    Part 2 lesson 4 completed.

--- lesson_four/main/main.py
+++ lesson_four/main/main.py
@@ -1,22 +1,25 @@
 # Lesson 4 Hands-On with Dictionaries
-fido = {
-    'Name': 'Fido',
-    'Type': 'Dog',
-    'Color': 'Black',
-    'Nickname': 'DoeDoe',
-    'Owner': 'Joe'
-}
+# Part 1
+fido = dict(Name='Fido', Type='Dog', Color='Black', Nickname='DoeDoe', Owner='Joe')
 
-charlie = {
-    'Name': 'Charlie',
-    'Type': 'Turtle',
-    'Color': 'Green',
-    'Nickname': 'Slow Poke',
-    'Owner': 'John'
-}
+charlie = dict(Name='Charlie', Type='Turtle', Color='Green', Nickname='Slow Poke', Owner='John')
 
 pets = [fido, charlie]
 
 for pet in pets:
     for key in pet:
         print('{} : {}'.format(key, pet[key]))
+
+# Part 2
+
+england = dict(Capital='London', Population='53.01 million', Fact='Still has a Queen', Language='English')
+
+france = dict(Capital='Paris', Population='66.9 million', Fact='Has the Eiffel Tower', Language='French')
+
+belgium = dict(Capital='Brussels', Population='11.35 million', Fact='Hosted the 1920 Summer Olympics', Language='Dutch')
+
+countries = [england, france, belgium]
+
+for county in countries:
+    for info in county:
+        print('{} : {}'.format(info, county[info]))
