60bc3bd81eeb8be35febeda97626fb1525355ad6
Hyrschall <Hyrschall@gmail.com>
2019-03-01 14:55:19 -0700

    Page 8 Modifying Files

--- lesson_seven/main/lesson_seven_pages.py
+++ lesson_seven/main/lesson_seven_pages.py
@@ -1,8 +1,22 @@
 # L7 Standard Library Testing
 # Page 6
-import math, random
+#import math, random
 
-my_random = random.random()*100
+#my_random = random.random()*100
 
-square_root = math.sqrt(my_random)
+#square_root = math.sqrt(my_random)
 
+# Page 8
+file = open('example.txt', 'w')
+
+print('File Name:', file.name)
+print('File Open Mode:', file.mode)
+
+def status(x):
+    if (x.closed != False):
+        return 'Closed'
+    else:
+        return 'Open'
+
+
+print('File Status:', status(file))
