692836a84aa894e4dbf58031f5d87bf09b4cc8d6
Hyrschall <Hyrschall@gmail.com>
2019-03-02 15:21:35 -0700

    Lesson 9 Part 1 completed, also code for other pages in lesson

--- lesson_nine/main/lesson_nine_pages.py
+++ lesson_nine/main/lesson_nine_pages.py
@@ -1,10 +1,68 @@
 # Lesson 9 Pages Debugging and Unit Testing in Python
-import pdb
-pdb.set_trace()
+# import unittest
+# import pdb
+# pdb.set_trace()
 
-def add_numbers(a, b):
-    """This function adds a to b."""
-    c = a + b
-    print(c)
+# Page 2
+# def add_numbers(a, b):
+#     """This function adds a to b."""
+#     c = a + b
+#     print(c)
+#
+#
+# add_numbers(3, 4)
 
-add_numbers(3, 4)
+# Page 3
+# dog = "Zorro"
+# cat = "Tesla"
+#
+# play = "My dog " + dog + " plays with my cat " + cat + " all the time."
+#
+# print(play)
+
+
+# Page 5
+# def format_name(first_name, last_name):
+#     """Will neatly format a full name."""
+#     full_name = first_name + ' ' + last_name
+#     return full_name.title()
+# def format_name(first_name, middle_name, last_name):
+#     """Will neatly format a full name."""
+#     full_name = first_name + ' ' + middle_name + ' ' + last_name
+#     return full_name.title()
+# def format_name(first_name, last_name, middle_name=''):
+#     """Will neatly format a full name."""
+#     if middle_name:
+#         full_name = first_name + ' ' + middle_name + ' ' + last_name
+#     else:
+#         full_name = first_name + ' ' + last_name
+#     return full_name.title()
+#
+#
+# class FormatNamesTest(unittest.TestCase):
+#     """Tests format_name() function."""
+#
+#     def test_format_name(self):
+#         """Will the name 'James Jones' work?"""
+#         name_formatted = format_name('james', 'jones')
+#         self.assertEqual(name_formatted, 'James Jones')
+#
+#     def test_full_name(self):
+#         """Will the name 'John Paul Jones' work?"""
+#         name_formatted = format_name('john', 'jones', 'paul')
+#         self.assertEqual(name_formatted, 'John Paul Jones')
+#
+#
+# unittest.main()
+
+# print('To quit at anytime enter "q".')
+# while True:
+#     first_name = input("\nPlease enter your first name: ")
+#     if first_name == 'q':
+#         break
+#     last_name = input("Please enter your last name: ")
+#     if last_name == 'q':
+#         break
+#
+#     formatted_name = format_name(first_name, last_name)
+#     print("\tHere is your name neatly formatted: " + formatted_name + '.')
--- lesson_nine/main/main.py
+++ lesson_nine/main/main.py
@@ -1,2 +1,18 @@
 # Lesson 9 Debugging and Unit Testing in Python
-import pdb
+import unittest
+
+
+def multiply_numbers(a, b):
+    """This Function Multiplies Two Numbers Together And Stores The Result."""
+    result = a * b
+    return result
+
+
+class TestMultiplyingNumbers(unittest.TestCase):
+    """ Testing multiplyed numbers function"""
+
+    def test_numbers(self):
+        """Do you have enough numbers?"""
+        numbers_multiplied = multiply_numbers(3, 4)
+        self.assertEqual(numbers_multiplied, 12)
+
